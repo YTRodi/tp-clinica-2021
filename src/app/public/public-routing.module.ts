4c799732c6ac6c8355315a4e705b05cf20b6371c
YTRodi <yagorodi@gmail.com>
2021-11-07 19:27:35 -0300

    add login and register components

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from '../shared/pages/not-found/not-found.component';
 
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
-  // { path: '404', component: NotFoundComponent },
-  // { path: '**', redirectTo: '/404' },
-  { path: '', component: HomeComponent },
-  { path: 'auth/login', component: NotFoundComponent },
-];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
